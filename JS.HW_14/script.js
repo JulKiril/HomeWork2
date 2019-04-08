0250d2362fface530d85bbacaddb391fa8dc41fb
Maksym Kyrylenko <maksym.kyrylenko@synapse.com>
2019-04-08 13:22:05 +0300

    changed gs file

--- JS.HW_14/script.js
+++ JS.HW_14/script.js
@@ -9,26 +9,19 @@ addF.addBtn.addEventListener('click', function () {
     }
     cenzorWords.innerText += ` ` + newW;
 })
-
 let czF = document.forms['cz'];
 czF.czBtn.addEventListener('click', function () {
     let arr1 = cenzorWords.innerText.split(' ');
-    let arr2 = czF.txt.value.split(' ');
+    let str = czF.txt.value;
     let repl = "*";
-    for (let i = 0; i < arr2.length; i++) {
-        for (let j = 0; j < arr1.length; j++) {
-            if (arr2[i] == arr1[j]) {
-                let strRepl = "";
-                for (let k = 0; k < arr2[i].length; k++) {
-                    strRepl += repl;
-                }
-                arr2[i] = strRepl;
+    for (let i = 0; i < arr1.length; i++) {
+            let strRepl = "";
+            for (let k = 0; k < arr1[i].length; k++) {
+                strRepl += repl;
             }
-
-        }
-    }
-    let str = arr2.join(' ');
-    czF.txt.value = str;
-    console.log(str);
+            str = str.replace(arr1[i], strRepl);
+        }    
+czF.txt.value = str;
+console.log(str);
 })
 
