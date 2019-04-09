b30902e25e5e2dda6b246834c9a1116323449714
Maksym Kyrylenko <maksym.kyrylenko@synapse.com>
2019-04-09 18:25:37 +0300

    added HW.JS_15

---
+++ JS.HW_15_1/script.js
@@ -0,0 +1,86 @@
+let getId = x => document.getElementById(x);
+let rightBtn = getId('rightBtn');
+let leftBtn = getId('leftBtn');
+let slider = getId('slider');
+let playBtn = getId('playBtn');
+let form = document.forms['sliderDots'];
+let imgArr = [];
+let direction;
+let over =  getId('over');
+for (let i = 0; i <= 9; i++) {
+    imgArr[i] = `img/${i}.jpg`;
+}
+imgArr.shift();
+let start =1;
+console.log(imgArr);
+let check = true;
+let mySlider;
+playBtn.addEventListener('click', function () {
+    if (check) {
+        playBtn.value = `pause`;
+        mySlider = setInterval(moveLeft, 2000);
+        check = false;
+    }
+    else {
+        playBtn.value = `play`;
+        check = true;
+        clearInterval(mySlider);
+    }
+})
+
+
+for (i = 0; i < form.point.length; i++) {
+    form.point[i].index = i;
+     form.point[i].addEventListener('mouseover', function(){
+        console.log(event.target);
+        over.classList.add('dspBlock');
+        // over.style.background = `url(${imgArr[event.target.index]})`;
+    })
+    form.point[i].addEventListener('click', function () {
+        slider.style.background = `url(${imgArr[event.target.index]})`;
+        slider.style.backgroundSize = `100% 100%`;
+        slider.style.backgroundRepeat = `no-repeat`;
+        console.log(event.target.index);
+        start = event.target.index + 1;         
+        console.log(start); 
+        rightBtn.addEventListener('click', moveLeft);        
+    })
+   
+}
+rightBtn.addEventListener('click', moveLeft);
+function moveLeft() {
+    if(direction === "left"){
+        start++;
+    }
+    direction = 'right';
+     if (start == imgArr.length) {
+        start = 0;
+    } 
+    console.log(start);        
+    slider.style.background = `url(${imgArr[start]})`;
+    slider.style.backgroundSize = `100% 100%`;
+    slider.style.backgroundRepeat = `no-repeat`;
+    form.point[start].checked = true;    
+    start++;   
+    console.log(start);
+    leftBtn.addEventListener('click', moveRight);     
+}  
+function moveRight(){
+    if(direction === "right"){
+        start--;        
+    }
+    direction = "left";
+    if (start == 0) {
+        start = imgArr.length;
+    } 
+    console.log(start);
+    start--;
+    console.log(start);
+    console.log(start);
+    slider.style.background = `url(${imgArr[start]})`;
+    slider.style.backgroundSize = `100% 100%`;
+    slider.style.backgroundRepeat = `no-repeat`;
+    form.point[start].checked = true;
+}
+
+
